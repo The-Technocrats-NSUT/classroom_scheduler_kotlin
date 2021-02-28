605ea8d41c4604d5850ba9ea05e4de3b9adb3437
AkinchanKushwaha <akinchandroger@gmail.com>
2021-02-28 14:13:28 +0530

    add ClassroomActivity.kt and ClassroomHubActivity.kt

---
+++ app/src/main/java/com/example/classroomScheduler/ClassroomHubActivity.kt
@@ -0,0 +1,11 @@
+package com.example.classroomScheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClassroomHubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_classroom_hub)
+    }
+}
---
+++ app/src/main/java/com/example/classroomScheduler/LectureFragment.kt
@@ -0,0 +1,8 @@
+package com.example.classroomScheduler
+
+import androidx.fragment.app.Fragment
+import com.example.classroomScheduler.R
+
+class LectureFragment : Fragment(R.layout.fragment_lecture) {
+
+}
