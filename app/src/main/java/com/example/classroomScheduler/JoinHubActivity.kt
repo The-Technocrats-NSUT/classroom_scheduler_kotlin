f0548757356a7e6265b2f7256b0d88addc810b95
Aman Kumar <kaman7580@gmail.com>
2021-05-19 20:02:48 +0530

    Upcoming Model

---
+++ app/src/main/java/com/example/classroomScheduler/JoinHubActivity.kt
@@ -0,0 +1,11 @@
+package com.example.classroomScheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinHubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_hub)
+    }
+}
---
+++ app/src/main/java/com/example/classroomScheduler/Utils/PostUpcomingEvent.kt
@@ -0,0 +1,11 @@
+package com.example.classroomScheduler.Utils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostUpcomingEvent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_upcoming_event)
+    }
+}
